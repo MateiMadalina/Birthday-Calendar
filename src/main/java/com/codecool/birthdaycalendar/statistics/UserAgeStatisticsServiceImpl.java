ce7aec5d4294b348f4bb43297b7890c467c7a510
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-07-20 18:30:53 +0300

    Add the class UserAgeStatisticsServiceImpl

---
+++ src/main/java/com/codecool/birthdaycalendar/statistics/UserAgeStatisticsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.codecool.birthdaycalendar.statistics;
+
+import com.codecool.birthdaycalendar.users.User;
+
+import java.util.List;
+import java.util.Map;
+
+public class UserAgeStatisticsServiceImpl implements UserAgeStatisticsService{
+    @Override
+    public List<User> getOldestUsers() {
+        return null;
+    }
+
+    @Override
+    public List<User> getYoungestUsers() {
+        return null;
+    }
+
+    @Override
+    public Map<Integer, List<User>> getUsersWithSameAgeInYears() {
+        return null;
+    }
+
+    @Override
+    public Map<Integer, List<User>> getUsersWithSameAgeInDays() {
+        return null;
+    }
+}
